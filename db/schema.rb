44c091de2a3866b04a10bb2aa5b7a9b4ce492291
Taro Matsuzawa <taro@georepublic.co.jp>
2014-02-08 23:57:14 +0900

    add session

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140208070335) do
+ActiveRecord::Schema.define(:version => 20140208112245) do
 
   create_table "admin_users", :force => true do |t|
     t.string   "name"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(:version => 20140208070335) do
     t.integer  "parent_right"
     t.datetime "created_at",                                                                  :null => false
     t.datetime "updated_at",                                                                  :null => false
+  end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
